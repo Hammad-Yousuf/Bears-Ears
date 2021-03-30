8c195bfb36f4069a45892abf9bce4d26eb7a92ed
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2021-03-30 21:19:55 +0500

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,29 @@
-document.getElementById('skip').addEventListener('click', ()=>{
-    document.getElementById("video").style.display = 'none';
+// First Modal
+
+document.getElementById('_first').addEventListener('click', ()=>{
+    document.getElementById('_first-modal').classList.add('appear-modal')
+})
+
+document.getElementById('_close-modal').addEventListener('click', ()=>{
+    document.getElementById('_first-modal').classList.remove('appear-modal')
+})
+
+// Second Modal
+
+document.getElementById('_second').addEventListener('click', ()=>{
+    document.getElementById('_second-modal').classList.add('appear-modal')
+})
+
+document.getElementById('_second-close-modal').addEventListener('click', ()=>{
+    document.getElementById('_second-modal').classList.remove('appear-modal')
+})
+
+// Third Modal
+
+document.getElementById('_third').addEventListener('click', ()=>{
+    document.getElementById('_third-modal').classList.add('appear-modal')
+})
+
+document.getElementById('_third-close-modal').addEventListener('click', ()=>{
+    document.getElementById('_third-modal').classList.remove('appear-modal')
 })
